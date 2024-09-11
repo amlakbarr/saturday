7d8f3ab3bc2ea1d84196018abeb8ef1a34eb381e
pistaacademy <pista.academy@gmail.com>
2024-09-11 07:30:45 +0330

    Version-1

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const Data = [
+    {
+        id: 1,
+        title: 'Prison Song',
+        artist: 'graham Nash',
+        thumbnail: require('./images/prison.jpeg'),
+        src: require('./song/Graham_Nash_Prison_Song.mp3')
+    },
+    {
+        id: 2,
+        title: 'Gheshang',
+        artist: 'Heydoo',
+        thumbnail: require('./images/Gheshang.jpeg'),
+        src: require('./song/Heydoo_Gheshang.mp3')
+    },
+    {
+        id: 3,
+        title: 'Ahoo',
+        artist: 'Juma',
+        thumbnail: require('./images/ahoo.jpg'),
+        src: require('./song/juma_ahoo.mp3')
+    },
+    {
+        id: 4,
+        title: 'Ashegh Bodam',
+        artist: 'Naser Abdolahi',
+        thumbnail: require('./images/Ashegh.jpeg'),
+        src: require('./song/Naser_Abdolahi_Ashegh_Bodam.mp3')
+    },
+    {
+        id: 5,
+        title: 'Son Arzum',
+        artist: 'Nilufer',
+        thumbnail: require('./images/Arzum.jpeg'),
+        src: require('./song/Nilufer_Son_Arzum.mp3')
+    },
+]
+
+export default Data;
